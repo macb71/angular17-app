fb0e5315d683c0fd7934b467d61055a409f38f47
MacB71 <macb71@gmail.com>
2023-12-11 18:26:04 +0100

    feat: add cors test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+@Injectable({ providedIn: 'root' })
+export class AppComponent implements OnInit {
   title = 'angulat17-app';
+  text = '';
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit(): Promise<void> {
+    this.text = await this.getResFromDoggieNote();
+  }
+
+  async getResFromDoggieNote(): Promise<string> {
+  
+    const response = await fetch('http://localhost:3000', {});
+    return await response.text();
+  }
 }
